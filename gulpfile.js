4b836f7deb2aca3f9334fdff13a73f8e2e7398a6
Axel Moncayo <axel.moncayo.dev@gmail.com>
2023-11-20 22:01:14 -0600

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,38 @@
+const {src, dest, watch} =  require("gulp");
+const sass = require("gulp-sass")(require("sass"));
+const imagemin = require("gulp-image");
+const plumber = require("gulp-plumber");
+const sourcemaps = require("gulp-sourcemaps");
+const postcss = require("gulp-postcss");
+const cssnano = require("cssnano");
+const autoprefixer = require("autoprefixer");
+
+function css(done){
+    src("src/scss/**/*.scss")
+        .pipe(sourcemaps.init())
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(postcss([autoprefixer(), cssnano()]))
+        .pipe(sourcemaps.write())
+        .pipe(dest("build/css"))
+
+    done()
+}
+
+function images(done){
+    src("src/img/**/*.{jpg,png}'")
+        .pipe(imagemin({optimizationLevel: 3}))
+        .pipe(dest("build/img"))
+
+    done()
+}
+
+function dev(done){
+    watch("src/scss/**/*.scss", css)
+
+    done()
+}
+
+exports.css = css;
+exports.images = images;
+exports.dev = series(css, dev);
---
+++ steps.md
@@ -0,0 +1,13 @@
+# Install SASS and Gulp steps 
+(You need to have node.js in your computer)
+
+1. npm init
+2. npm i -D gulp
+3. npm i -D gulp-sass
+4. npm i -D sass
+5. npm i -D gulp-imagemin@7.1.0
+6. npm i -D gulp-plumber
+7. npm i -D gulp-postcss
+8. npm i -D gulp-sourcemaps
+9. npm i -D cssnano
+10. npm i -D autoprefixer
